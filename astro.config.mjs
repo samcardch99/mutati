5f732fc589df76410ec74d0e7e6bb37874792ef4
Samuel Cardenas <samcard1999@gmail.com>
2026-02-14 09:19:01 -0500

    ss

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import lenis from 'astro-lenis';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [lenis()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
