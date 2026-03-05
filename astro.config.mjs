33fdae9bb354118032c92195bb0cba814ea1be52
Samuel Cardenas <samcard1999@gmail.com>
2026-03-05 15:45:55 -0500

    last

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,14 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp', // Usar Sharp para máxima calidad
+      config: {
+        limitInputPixels: false, // Permite procesar imágenes muy pesadas
+      },
+    },
+  },
   integrations: [lenis(), react()],
   vite: {
     plugins: [tailwindcss()]
