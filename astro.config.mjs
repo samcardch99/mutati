62620fb321dc02fc2f0cf718b84bb5f672f3a020
Samuel Cardenas <samcard1999@gmail.com>
2026-03-10 10:28:54 -0400

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import lenis from 'astro-lenis';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mutatidesign.com',
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp', // Usar Sharp para máxima calidad
@@ -16,7 +16,7 @@ export default defineConfig({
       },
     },
   },
-  integrations: [lenis(), react()],
+  integrations: [lenis(), react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
